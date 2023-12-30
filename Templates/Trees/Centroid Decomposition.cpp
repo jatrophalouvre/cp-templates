480c8996ac1c47f6d5abdd512a73777fbd95c44e
user123 <73287587+user-12313@users.noreply.github.com>
2023-12-30 10:49:44 +0800

    Update Centroid Decomposition.cpp

--- Templates/Trees/Centroid Decomposition.cpp
+++ Templates/Trees/Centroid Decomposition.cpp
@@ -2,12 +2,12 @@
 // to be reviewed
 
 #include <iostream>
-#include <cstring>
+#include <vector>
 using namespace std;
 
 const int N=100013, M=200013, L=23;
 int n, m;
-int h[N], e[M], ne[M], idx=0;
+vector<int> s[N];
 int d[N][L], ans[N];
 int c[N][L], lvl[N];
 
@@ -27,17 +27,12 @@ struct node
         int cent=get(u, u, sz[u]);
         lvl[cent]=dep;
         dfs_c(cent, cent, dep, cent, 0);
-        for (int a=h[cent]; ~a; a=ne[a])
-        {
-            int v=e[a];
-            if (lvl[v]==-1) decomp(v, cent, dep+1);
-        }
+        for (auto v:s[cent]) if (lvl[v]==-1) decomp(v, cent, dep+1);
     }
     int get(int u, int p, const int tsz)
     {
-        for (int a=h[u]; ~a; a=ne[a])
+		for (auto v:s[u])
         {
-            int v=e[a];
             if (v==p || lvl[v]!=-1) continue;
             if (tsz<sz[v]*2)
             {
@@ -49,9 +44,8 @@ struct node
     void dfs_sz(int u, int p)
     {
         sz[u]=1;
-        for (int a=h[u]; ~a; a=ne[a])
+        for (auto v:s[u])
         {
-            int v=e[a];
             if (v==p || lvl[v]!=-1) continue;
             dfs_sz(v, u);
             sz[u]+=sz[v];
@@ -60,9 +54,8 @@ struct node
     void dfs_c(int u, int p, const int level, const int cent, int dep)
     {
         c[u][level]=cent; d[u][level]=dep;
-        for (int a=h[u]; ~a; a=ne[a])
+        for (auto v:s[u])
         {
-            int v=e[a];
             if (v==p || lvl[v]!=-1) continue;
             dfs_c(v, u, level, cent, dep+1);
         }
@@ -71,13 +64,12 @@ struct node
 
 int main()
 {
-    memset(h, -1, sizeof(h));
     cin>>n>>m;
     for (int a=0; a<n-1; a++)
     {
         int x, y; cin>>x>>y;
-        e[idx]=y; ne[idx]=h[x]; h[x]=idx++;
-        e[idx]=x; ne[idx]=h[y]; h[y]=idx++;
+        s[x].push_back(y);
+        s[y].push_back(x);
     }
     
     centd=new node();
